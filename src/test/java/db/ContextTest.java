c81c0382bd463b408e9846f406074ea8b3a69756
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-08-17 10:04:17 -0300

    Using jpa extras

--- src/test/java/db/ContextTest.java
+++ src/test/java/db/ContextTest.java
@@ -3,16 +3,19 @@ package db;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
-public class ContextTest {
+public class ContextTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
 
 	@Test
 	public void contextUp() {
-		EntityManagerHelper.getEntityManager();
+		assertNotNull(entityManager());
 	}
 
 	@Test
 	public void contextUpWithTransaction() throws Exception {
-		EntityManagerHelper.withTransaction(() -> {});
+		withTransaction(() -> {});
 	}
+
 }
