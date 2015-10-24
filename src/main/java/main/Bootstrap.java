9cf956f51e6cb959e437839ff20564c96763a389
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-10-24 20:14:32 -0300

    Agregado layout

--- src/main/java/main/Bootstrap.java
+++ src/main/java/main/Bootstrap.java
@@ -6,10 +6,12 @@ import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 
-public class Bootstrap implements
-  WithGlobalEntityManager, 
-  EntityManagerOps, 
-  TransactionalOps {
+/**
+ * Ejecutar antes de levantar el servidor por primera vez
+ * 
+ * @author flbulgarelli
+ */
+public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, TransactionalOps {
 
   public static void main(String[] args) {
     new Bootstrap().run();
