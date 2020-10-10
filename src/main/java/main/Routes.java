b2dedc85518d3c2bd1e8dbe6e62021c0f55ce3c2
Franco Bulgarelli <franco@mumuki.org>
2020-10-09 21:14:57 -0300

    Agregando primera ruta con spark y handlebars

--- src/main/java/main/Routes.java
+++ src/main/java/main/Routes.java
@@ -5,12 +5,19 @@ import static spark.Spark.post;
 import static spark.SparkBase.port;
 
 import spark.ModelAndView;
+import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class Routes {
 
   public static void main(String[] args) {
-
+    Spark.port(8080);
+    
+    HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+    
+    Spark.get("/", (request, response) -> {
+        return new ModelAndView(null, "index.html.hbs");
+    }, engine);
   }
 
 }
