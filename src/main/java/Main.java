3b484b7c4475b3691126689e338200f09b5181c7
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-10-08 15:28:10 -0300

    Add template engine

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class Main {
   public static void main(String[] args) {
     Spark.port(9000);
-    Spark.get("/", (request, response) ->
-                      "<h1>¡Hola " + request.queryParamOrDefault("nombre", "Mundo")+ "!</h1>"
-                    + "<p>Esta es nuestra primera respuesta HTML</p>");
+    HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+
+    Spark.get("/",
+            (request, response) ->
+                    new ModelAndView( //1
+                            request.queryParamOrDefault("nombre", "Mundo"),
+                            "bienvenida.html.hbs"), //2
+            engine); //3
   }
 }
