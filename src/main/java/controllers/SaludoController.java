f9e908c78c76996447d18c04e4adb371083c33d4
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-10-09 20:52:28 -0300

    Remove duplicated logic

--- src/main/java/controllers/SaludoController.java
+++ src/main/java/controllers/SaludoController.java
@@ -6,14 +6,14 @@ import spark.Response;
 
 public class SaludoController {
   public ModelAndView bienvenida(Request request, Response response) {
-    return new ModelAndView(
-            request.queryParamOrDefault("nombre", "Mundo"),
-            "bienvenida.html.hbs");
+    return new ModelAndView(leerNombre(request), "bienvenida.html.hbs");
   }
 
   public ModelAndView despedida(Request request, Response response) {
-    return new ModelAndView(
-            request.queryParamOrDefault("nombre", "Mundo"),
-            "despedida.html.hbs");
+    return new ModelAndView(leerNombre(request), "despedida.html.hbs");
+  }
+
+  public String leerNombre(Request request) {
+    return request.queryParamOrDefault("nombre", "Mundo");
   }
 }
