e3bd3d8a6eff2957b926f945229ac900a74fd6f0
Franco Leonardo Bulgarelli <franco@mumuki.org>
2023-08-19 22:23:06 -0300

    Ejemplo en clase

--- src/main/java/utn/dds/persistencia/futbol/persistence/Jugador.java
+++ src/main/java/utn/dds/persistencia/futbol/persistence/Jugador.java
@@ -1,9 +1,28 @@
 package utn.dds.persistencia.futbol.persistence;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Jugador {
+
+	@Id
+	// Nota: observar cómo cambia el esquema y cómo cambia el orden
+	// de las operaciones al correr el ContextTest usando una u otra
+	// estrategia
+	// @GeneratedValue(strategy = GenerationType.IDENTITY)
+	// @GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue
+	private Long id;
 	
 	private String nombre;
 	private String posicion;
+
+	public Long getId() {
+		return id;
+	}
 
 	public String getNombre() {
 		return nombre;
--- src/test/java/utn/dds/persistencia/futbol/ContextTest.java
+++ src/test/java/utn/dds/persistencia/futbol/ContextTest.java
@@ -19,6 +19,30 @@ public class ContextTest implements SimplePersistenceTest {
     Jugador dani = new Jugador();
     dani.setNombre("Dani");
     dani.setPosicion("11");
-  
+
+    System.out.println("Haciendo persistible");
+    entityManager().persist(dani);
+    assertEquals((Long) 1L, dani.getId());
+    dani.setPosicion("9");
+
+    System.out.println("Buscando");
+    Jugador dani2 = entityManager().find(Jugador.class, 1L);
+
+    assertNotNull(dani2);
+    assertEquals(dani.getId(), dani2.getId());
+    assertEquals("9", dani2.getPosicion());
+    assertSame(dani, dani2);
+
+    System.out.println("Sincronizando");
+    entityManager().flush();  // No debería estar en su código, sino en un lugar muy especial
+    entityManager().clear();  // idem
+
+    System.out.println("Buscando nuevamente");
+    Jugador dani3 = entityManager().find(Jugador.class, 1L);
+
+    assertNotNull(dani3);
+    assertEquals(dani.getId(), dani3.getId());
+    assertEquals("9", dani3.getPosicion());
+    assertNotSame(dani, dani3);
   }
 }
