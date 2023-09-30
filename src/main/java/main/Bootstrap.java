995a2286ca780f618a44e96721eaac917902bdb0
Franco Leonardo Bulgarelli <franco@mumuki.org>
2023-09-29 21:46:38 -0300

    Cargar consultoras sólo si la base está vacía

--- src/main/java/main/Bootstrap.java
+++ src/main/java/main/Bootstrap.java
@@ -3,11 +3,9 @@ package main;
 import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import model.Consultora;
 
-import model.Usuario;
-
 /**
  * Ejecutar antes de levantar el servidor por primera vez
- * 
+ *
  * @author flbulgarelli
  */
 public class Bootstrap implements WithSimplePersistenceUnit {
@@ -18,15 +16,17 @@ public class Bootstrap implements WithSimplePersistenceUnit {
 
   public void run() {
     withTransaction(() -> {
-      persist(new Consultora("dblandit", 10));
-      persist(new Consultora("2diseños", 15));
-      persist(new Consultora("chakanalabs", 2));
-      persist(new Consultora("consultoraLoca", 10));
-      persist(new Consultora("rolicusulting", 20));
-      persist(new Consultora("francoconsulting", 2));
-      persist(new Consultora("otraConsultora", 10));
-      persist(new Consultora("pirulo.com", 15));
-      persist(new Consultora("blah", 2));
+      if (entityManager().createQuery("from Consultora").getResultList().isEmpty()) {
+        persist(new Consultora("dblandit", 10));
+        persist(new Consultora("2diseños", 15));
+        persist(new Consultora("chakanalabs", 2));
+        persist(new Consultora("consultoraLoca", 10));
+        persist(new Consultora("rolicusulting", 20));
+        persist(new Consultora("francoconsulting", 2));
+        persist(new Consultora("otraConsultora", 10));
+        persist(new Consultora("pirulo.com", 15));
+        persist(new Consultora("blah", 2));
+      }
     });
   }
 
