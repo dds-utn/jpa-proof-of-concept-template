ec40d8836238d85401a4217e65237d04537136a3
Franco Leonardo Bulgarelli <franco@mumuki.org>
2026-08-28 21:38:26 -0300

    Versión con table per class

--- src/main/java/utn/dds/persistencia/biblioteca/Publicacion.java
+++ src/main/java/utn/dds/persistencia/biblioteca/Publicacion.java
@@ -4,11 +4,11 @@ import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Publicacion {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.TABLE)
   private Long id;
 
   private String titulo;
